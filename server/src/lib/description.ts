be7ed3878161693c4fc8df3a15fdfc10e99eb990
TheHyrox <thehyrox@gmail.com>
2026-03-29 11:51:28 +0200

    feat: enhance Pokemon description handling with localization and deduplication

--- server/src/lib/description.ts
+++ server/src/lib/description.ts
@@ -28,8 +28,66 @@ export async function getPokemonDescription(pokemon : string, lang: string, gene
             }
         });
 
-        return descriptions;
+        return await noSpoilerDescription(descriptions, lang, data);
     } catch (e) {
         console.log("Got an error: ", e);
     }
+}
+
+async function noSpoilerDescription(descriptions : string[], lang: string, data: any) {
+    let nameLocalized = data.names.find((name: { language: { url: string; }; }) => name.language.url.endsWith(`/${lang}/`));
+    let placeHolder = '';
+
+    switch (lang) {
+        case "1":
+            placeHolder = "このポケモン";
+            break;
+        case "3": 
+            placeHolder = "이 포켓몬";
+            break;
+        case "4":
+            placeHolder = "這隻寶可夢";
+            break;
+        case "5":
+            placeHolder = "ce Pokémon";
+            break;
+        case "6":
+            placeHolder = "dieses Pokémon";
+            break;
+        case "7":
+            placeHolder = "este Pokémon";
+            break;
+        case "8":
+            placeHolder = "questo Pokémon" 
+            break;
+        case "9":
+            placeHolder = "this pokemon";
+            break;
+        case "11":
+            placeHolder = "このポケモン";
+            break;
+        case "12":
+            placeHolder = "这只宝可梦";
+            break;
+        default:
+            placeHolder = "this pokemon";
+    }
+
+    for (let i = 0; i < descriptions.length; i++) {
+        descriptions[i] = descriptions[i].replace(new RegExp(nameLocalized.name, 'gi'), placeHolder);
+    }
+
+    return await deleteDupplicateDescriptions(descriptions);
+}
+
+async function deleteDupplicateDescriptions(descriptions : string[]) {
+    let uniqueDescriptions : string[] = [];
+
+    descriptions.forEach(description => {
+        if (!uniqueDescriptions.includes(description)) {
+            uniqueDescriptions.push(description);
+        }
+    });
+
+    return uniqueDescriptions;
 }
